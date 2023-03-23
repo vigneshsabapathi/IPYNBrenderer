d6f6fbbad86181fb8a64c1d5770c8bc052654ddd
vigneshsabapathi <vignesh.sabapathi@gmail.com>
2023-03-23 12:32:23 +0530

    updated setup

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Createing conda environment with python 3.8"
-conda create -p venv python==3.8 -y
-echo [$(date)]: "Activating conda environment"
-source activate venv/
-echo [$(date)]: "Installing Requirements"
+echo [$(date)]: "Creating conda env with python 3.8"
+conda create --prefix ./venv python=3.8 -y
+echo [$(date)]: "activate env"
+source activate ./venv
+echo [$(date)]: "intalling dev requirements"
 pip install -r requirements_dev.txt
-echo [$(date)]: "END"    
+echo [$(date)]: "END"
