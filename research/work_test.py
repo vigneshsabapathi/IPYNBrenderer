09cb571e44c96834a9c3b1b918a6578251a9618e
vigneshsabapathi <vignesh.sabapathi@gmail.com>
2023-04-03 21:22:12 +0530

    test updates

---
+++ research/work_test.py
@@ -0,0 +1,12 @@
+def anagram(words):
+    anagrams = {}
+    for word in words:
+        sortedword = "".join(sorted(word))
+        if sortedword in anagrams:
+            anagrams[sortedword].append(word)
+        else:
+            anagrams[sortedword] = [word]
+    return list(anagrams.values())
+ 
+words = ["cinema","iceman","foo","oof"]
+print(anagram(words))
